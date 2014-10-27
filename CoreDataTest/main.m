4f5bb4edfcb65ceddf003cd0f435ee39d1637691
葛運昇 <ge-ys@atjava.com>
2014-10-27 12:18:43 +0800

    Initial Commit

---
+++ CoreDataTest/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  CoreDataTest
+//
+//  Created by Ge-Yunsheng on 2014/10/27.
+//  Copyright (c) 2014年 geys. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
